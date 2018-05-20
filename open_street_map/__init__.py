d30a009ca47d2723c94aead3bb50feb2b2aa1d69
houssamtrizi <houssam.trizi@gmail.com>
2018-05-20 16:47:53 +0000

    get population data

--- open_street_map/__init__.py
+++ open_street_map/__init__.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-DATA = pd.read_csv("Volcanoes_USA.txt")
+DATA_VOLCANO = pd.read_csv("Volcanoes_USA.txt")
+
+with open("world.json", encoding="utf-8-sig") as f:
+    DATA_POPULATION = f.read()
 
 MAP_FILE_PATH = "map.html"
